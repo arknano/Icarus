ce96cbeb2d743adc3720f423a79df3e447d06157
Jake Spence <jake.spence@ad.aie.edu>
2016-10-17 10:45:49 +1030

    Added Test programming files

---
+++ Assets/Scripts/Test Scripts/TimerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class TimerScript : MonoBehaviour 
+{
+	public Text timerText;
+	private int timer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Time.timeScale = 100f;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TimeSpan time = TimeSpan.FromSeconds(Time.timeSinceLevelLoad);
+        timerText.text = string.Format("{0:00}:{1:00}", time.Minutes + (60 * time.Hours), time.Seconds);
+    }
+}
