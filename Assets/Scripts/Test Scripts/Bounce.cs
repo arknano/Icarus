910662b80c44adcc6804a457d28e179422279221
Stuart Burfield <stuartanthonyb@gmail.com>
2016-10-19 12:01:59 +1030

    Improved bouncing, moved the damping to the Glide Controller so that it is applied right after velocity is applied

--- Assets/Scripts/Test Scripts/Bounce.cs
+++ Assets/Scripts/Test Scripts/Bounce.cs
@@ -5,32 +5,25 @@ public class Bounce : MonoBehaviour
 {
 	public float bounceIntensity = 50;
 	public float bounceDamping = 0.9f;
-
 	public string wallTag = "Ground";
 
-	private Rigidbody rb;
 	private GlideController glide;
 
 	// Use this for initialization
 	void Start () 
-	{
-		rb = GetComponent<Rigidbody>();
+	{		
 		glide = GetComponent<GlideController>();
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () 
-	{
-		glide.BounceVelocity *= 0.9f;
-	}
+	void FixedUpdate () {}
 
 	void OnCollisionEnter(Collision collision)
-	{
+	{		
 		if (collision.gameObject.tag == wallTag)
 		{
 			glide.BounceVelocity = bounceIntensity * collision.contacts[0].normal;
-			glide.acceleration *= 0.5f;
-			//rb.AddForce(50 * collision.contacts[0].normal, ForceMode.Impulse);    
+			glide.acceleration *= 0.5f;			 
 		}
 	}
 }
