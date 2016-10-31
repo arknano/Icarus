70f00030458ac112b5da6b7566c042590c299bc5
Jake Spence <jake.spence@ad.aie.edu>
2016-10-31 12:22:16 +1030

    Added a rotate script. Untested.

---
+++ Assets/Scripts/Test Scripts/RotateObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour
+{
+    float time;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        time = Time.deltaTime;
+        transform.Rotate(0, 50 * time, 50 * time); 	
+	}
+}
