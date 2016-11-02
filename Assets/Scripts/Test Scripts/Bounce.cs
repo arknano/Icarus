36b6d65f0e8ad9cde49e430aa654523dd32a32a1
Stuart Burfield <stuartanthonyb@gmail.com>
2016-11-02 11:59:44 +1030

    Added death debug.log and testing for normals with collisions with 'Ground' tagged objects so you know if you're hitting the grass or the underside of an island / object

--- Assets/Scripts/Test Scripts/Bounce.cs
+++ Assets/Scripts/Test Scripts/Bounce.cs
@@ -5,7 +5,8 @@ public class Bounce : MonoBehaviour
 {
 	public float bounceIntensity = 50;
 	//public float bounceDamping = 0.9f;
-	public string wallTag = "Ground";
+	public string obstacleTag = "Obstacle";
+	public string groundTag = "Ground";
 
 	private GlideController glide;
 
@@ -20,10 +21,25 @@ public class Bounce : MonoBehaviour
 
 	void OnCollisionEnter(Collision collision)
 	{		
-		if (collision.gameObject.tag == wallTag)
+		if (collision.gameObject.tag == groundTag )
+		{
+			if (collision.contacts[0].normal.y >= 0.3f)
+			{
+				Debug.Log("Death");
+				glide.BounceVelocity = bounceIntensity * Vector3.Reflect(glide.transform.forward, collision.contacts[0].normal);
+				glide.acceleration *= 0.5f;
+			}
+			else if (collision.contacts[0].normal.y < 0.3f)
+			{
+				Debug.Log("ALIVE!!!!!");
+				glide.BounceVelocity = bounceIntensity * Vector3.Reflect(glide.transform.forward, collision.contacts[0].normal);
+				glide.acceleration *= 0.5f;
+			}					 
+		}
+		else if (collision.gameObject.tag == obstacleTag)
 		{
 			glide.BounceVelocity = bounceIntensity * Vector3.Reflect(glide.transform.forward, collision.contacts[0].normal);
-			glide.acceleration *= 0.5f;			 
-		}
+			glide.acceleration *= 0.5f;	
+		}	
 	}
 }
